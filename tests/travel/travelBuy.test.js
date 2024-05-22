3a300ed338288fca92a695ff6da358351870af7f
meet marakana <meet.marakana@acko.tech>
2024-05-22 14:04:43 +0530

    adding travel buy journey test case

--- tests/travel/travelBuy.test.js
+++ tests/travel/travelBuy.test.js
@@ -10,6 +10,8 @@ describe('Travel Buy journey visual testing from App', () => {
     it('Travel bundled plan buy journey', async () => {
         let mobileNumber = pages.homePage.randomMobileNumber();
         await pages.homePage.login(mobileNumber);
-        await pages.homePage.sleep(10);
+        await pages.homePage.goToMyAccountsPage();
+        await pages.homePage.verifyNoPolicyText();
+        await pages.homePage.sleep(2);
     });
 });
