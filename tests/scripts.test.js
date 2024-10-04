af5d88ac54bc49ca9950707a8dad5ebdafaaabee
Nitin <nitin.kumar_codev_blr@ip-10-210-37-232.ap-south-1.compute.internal>
2024-10-04 17:18:06 +0530

    code

--- tests/scripts.test.js
+++ tests/scripts.test.js
@@ -6,21 +6,21 @@ const pages = {
 }
 
 describe('visual testing', () => {
-    it.only('Add KYC for a number', async () => {
-        let mobileNumber = "8892082613";
+    it('Add KYC for a number', async () => {
+        let mobileNumber = "6363967924";
         let name = "John Doe";
         await pages.commonScripts.addKYCSuccess(name, mobileNumber);
     });
 
     it('Back Date a retail travel policy', async () => {
         let data = {
-            mobileNumber : "8892082613",
+            mobileNumber : "6363967924",
             policyIndex : 0,
             isTravelDate : true,
-            travel_start_date : "06-09-2024",
-            travel_end_date : "20-09-2024",
-            policy_start_date : "2024-08-23T00:00:00.000+00:00",
-            policy_end_date : "2024-09-23T00:00:00.000+00:00",
+            travel_start_date : "01-10-2024",
+            travel_end_date : "30-10-2024",
+            policy_start_date : "2024-09-03T00:00:00.000+00:00",
+            policy_end_date : "2024-11-15T00:00:00.000+00:00",
         };
         await pages.commonScripts.backDatePolicy(data, data.policyIndex);
     });
